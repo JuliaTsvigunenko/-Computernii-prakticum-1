725bef34394b1a1b78ee766b660585aadd428835
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-03-05 20:36:17 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Практикум №1
+
+            // Задание 4
+            //int[] omas = new int[10];
+            //Random random = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    omas[i] = random.Next(1, 10);
+            //}
+            //foreach (int i in omas)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+
+            // Задание 1
+            //int[] omas = new int[6] { 6, 3, 7, 45, 59, 62 };
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+
+            // задание 2
+            //int[] omas = new int[11];
+            //Random random = new Random();
+            //for (int i = 0; i < omas.Length - 1; i++)
+            //{
+            //    omas[i] = random.Next(1, 10);
+            //}
+            //foreach (int i in omas)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
